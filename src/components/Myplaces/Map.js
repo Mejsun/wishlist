98973f8acdf88dfe7c2e9d7149a53545fa4c7d42
Mejsun <mejsunalghoul@gmail.com>
2021-12-17 11:27:57 +0000

    Styling chnges

--- src/components/Myplaces/Map.js
+++ src/components/Myplaces/Map.js
@@ -1,5 +1,5 @@
 import './Styles.scss'
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet'
 import {useState, useRef, useCallback, useMemo} from 'react'
 
 //to show your location
@@ -17,12 +17,8 @@ function LocationMarker() {
   )
 }
 
-//to drag the popup
-const center = {lat: 51.505,lng: -0.09}
-
 function Map ({tasks, setTasks}){
-
-
+  const center = {lat: 51.505,lng: -0.09}
   const [draggable, setDraggable] = useState(false)
   const [position, setPosition] = useState(center)
   const markerRef = useRef(null)
@@ -30,27 +26,28 @@ function Map ({tasks, setTasks}){
         if (marker != null) {setPosition(marker.getLatLng())}}}), [])
   const toggleDraggable = useCallback(() => {setDraggable((d) => !d)}, [])
 
-
     return (
         <div id='map'>
          <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <LocationMarker/>
-  
-  {tasks.map((item,i)=>{return(
-        <Marker draggable={draggable} eventHandlers={eventHandlers} position={center} ref={markerRef}>
-      <Popup minWidth={90}>
-        <span onClick={toggleDraggable} className={`markerPopup ${item.isCompleted  ? 'done' : ''}`}>
-      {i+1}. {item.text}
-        </span>
-      </Popup>
-    </Marker>
-      )})
-}
-</MapContainer>
+         <div className='searchbar'>search here</div>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <LocationMarker/>
+      
+    {tasks.map((item,i)=>{return(
+          <Marker draggable={draggable} eventHandlers={eventHandlers} position={center} ref={markerRef}>
+        <Popup minWidth={90}>
+          <span onClick={toggleDraggable} className={`markerPopup ${item.isCompleted  ? 'done' : ''}`}>
+        {i+1}. {item.text}
+          </span>
+        </Popup>
+      </Marker>
+        )})}
+     
+          </MapContainer>
         </div>
 )}
+
 export default Map
