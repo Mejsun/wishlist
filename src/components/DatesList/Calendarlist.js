7820b2f54062e95133382235d9ac0316cd7e9cfd
Mejsun <mejsunalghoul@gmail.com>
2022-01-16 20:53:18 +0000

    deleting events

--- src/components/DatesList/Calendarlist.js
+++ src/components/DatesList/Calendarlist.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Calendar, dateFnsLocalizer, Views} from 'react-big-calendar';
+import {Calendar, dateFnsLocalizer} from 'react-big-calendar';
 import format from 'date-fns/format';
 import parse from 'date-fns/parse';
 import startOfWeek from 'date-fns/startOfWeek';
@@ -26,20 +26,34 @@ const localizer = dateFnsLocalizer({
     locales
 })
 
-const events = [{}]
+const events = []
 
 function Calendarlist (){
     const [newEvent, setNewEvent] = useState({title:'', start:'', end:'', allDay: 'true'})
     const [allEvents, setAllEvents] = useState(events)
-    const addEvent = () => {setAllEvents([...allEvents, newEvent])}
-    console.log(Views.AGENDA)
+    const addEvent = () => {setAllEvents([...allEvents, newEvent]);}
+
+    let index = 0;
+    allEvents.forEach((newEvent) => {newEvent.index  = index++});
+
+    console.clear()
+    function deleteEvent(e){ 
+        let eventsdel = [...allEvents];
+        let idx = e.index
+        let id = parseInt(idx)
+        //console.log(id)
+        if (window.confirm(`${e.title} will be deleted!`)) {eventsdel.splice(id,1);}
+            else{return eventsdel}
+        setAllEvents(eventsdel)
+    }
+        
     return(
         <div className='calendar'>        
             <div className='list'>
                 <InputGroup className="mb-3 inputContainer">
                     <FormControl
                     aria-describedby="basic-addon2" placeholder='Add your event' value={newEvent.title} 
-                    onChange={(e)=> setNewEvent({...newEvent, title: e.target.value})}
+                    onChange={(e)=> setNewEvent({...newEvent, title: e.target.value })} 
                     />
                     <Button variant="outline-secondary" id="button-addon2" type='submit' onClick={addEvent} className='shadow-none'> <i className="fas fa-plus"></i></Button>
                 </InputGroup>
@@ -64,6 +78,9 @@ function Calendarlist (){
             endAccessor={({end}) => new Date(new Date(end).setHours(0,0, 0, 0) + 24 )}
             className='calendarMain'           
             views={['month', 'agenda']}
+            selectable
+            popup
+            onDoubleClickEvent={(e) => deleteEvent(e)}
             />
         </div>
     )
