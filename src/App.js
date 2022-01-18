9c93cb1b208aceb42ea67b16096520eeda3e6a63
Mejsun <mejsunalghoul@gmail.com>
2022-01-18 16:23:32 +0000

    homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Menu from './components/Menu.js'
+//import Home from './components/Home.js'
 
 function App() {
   console.log(React.version)
   return (
     <div>
-      <Menu/>
-     
+      <Menu/>      
     </div>
   )
 }
